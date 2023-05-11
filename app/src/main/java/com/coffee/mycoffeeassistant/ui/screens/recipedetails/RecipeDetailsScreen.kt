081a2425af9e7a42c9a4616d356b972812a704e7
Janek Łuczka <janekluczka@gmail.com>
2023-05-11 12:50:01 +0200

    added custom youtube video id to iframe player, screen view model

--- app/src/main/java/com/coffee/mycoffeeassistant/ui/screens/recipedetails/RecipeDetailsScreen.kt
+++ app/src/main/java/com/coffee/mycoffeeassistant/ui/screens/recipedetails/RecipeDetailsScreen.kt
@@ -9,41 +9,59 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.coffee.mycoffeeassistant.ui.AppViewModelProvider
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun RecipeDetailsScreen() {
+fun RecipeDetailsScreen(
+    recipeDetailsViewModel: RecipeDetailsViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+//    TODO: Change background color for iframe in dark mode
+
     val videoId = "tltBHjmIUJ0"
+    val iframeHtml = "<html>" +
+            "<meta name=\"viewport\" content=\"width=640\">" +
+            "<body style=\"margin: 0px; padding: 0px\">" +
+            "<iframe " +
+            "id=\"player\" " +
+            "frameborder=\"0\" " +
+            "allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" " +
+            "width=\"640\" " +
+            "height=\"360\" " +
+            "src=\"https://www.youtube.com/embed/${videoId}?version=3&amp;enablejsapi=1&amp;controls=1&amp;fs=0\">" +
+            "</iframe>" +
+            "</body>" +
+            "</html>"
 
-    val filePath = "file:///android_asset/iframeplayer.html"
+    Column() {
+        Box(modifier = Modifier.aspectRatio(16f / 9f)) {
+            AndroidView(
+                factory = {
+                    WebView(it).apply {
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                        )
+                        settings.apply {
+                            javaScriptEnabled = true
+                            loadWithOverviewMode = true
+                            useWideViewPort = true
+                        }
 
-    Box(
-        modifier = Modifier.aspectRatio(16f / 9f)
-    ) {
-        AndroidView(
-            factory = {
-                WebView(it).apply {
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                    )
-                    settings.apply {
-                        javaScriptEnabled = true
-//                        loadWithOverviewMode = true
-//                        useWideViewPort = true
+                        webViewClient = IFrameWebViewClient(it)
+                        loadData(iframeHtml, "text/html", "utf-8")
                     }
-                    webViewClient = IFrameWebViewClient(it)
-                    loadUrl(filePath)
+                },
+                update = {
                 }
-            },
-            update = {
-//                it.loadUrl(filePath)
-            }
-        )
+            )
+        }
     }
 }
 
---
+++ app/src/main/java/com/coffee/mycoffeeassistant/ui/screens/recipedetails/RecipeDetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.coffee.mycoffeeassistant.ui.screens.recipedetails
+
+import androidx.lifecycle.ViewModel
+
+class RecipeDetailsViewModel() : ViewModel() {
+
+
+
+}
